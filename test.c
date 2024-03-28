979a18674af4543fd8210a6353797ffaa1b15c83
xadicha <246abdurashidova123@gmail.com>
2024-03-28 11:33:09 +0500

    -t option worked

--- test.c
+++ test.c
@@ -36,8 +36,9 @@ int my_strcmp(char *str1, char *str2)
     return 0;
 }
 
-void add_node(list** head, char* name){
-    list* temp = (list*)malloc(sizeof(list));
+void add_node(list **head, char *name)
+{
+    list *temp = (list *)malloc(sizeof(list));
     temp->name = name;
     temp->next = NULL;
 
@@ -56,15 +57,17 @@ void add_node(list** head, char* name){
     }
 }
 
-void my_linked_list_swap(char** s1, char** s2){
-    char* tmp = *s1;
+void my_linked_list_swap(char **s1, char **s2)
+{
+    char *tmp = *s1;
     *s1 = *s2;
     *s2 = tmp;
 }
 
-void sort_by_ascii(list* head){
-    list* temp2 = NULL;
-    list* temp1 = head;
+void sort_by_ascii(list *head)
+{
+    list *temp2 = NULL;
+    list *temp1 = head;
 
     while (temp1 != NULL)
     {
@@ -81,9 +84,11 @@ void sort_by_ascii(list* head){
     }
 }
 
-void sort_by_ascii(list* head){
-    list* temp2 = NULL;
-    list* temp1 = head;
+void sort_by_time(list *head)
+{
+
+    list *temp2 = NULL;
+    list *temp1 = head;
 
     struct stat fs1;
     struct stat fs2;
@@ -95,53 +100,74 @@ void sort_by_ascii(list* head){
         {
             stat(temp1->name, &fs1);
             stat(temp2->name, &fs2);
-            if (fs1.st_mtime == fs2.st_mtime)
+            if (fs1.st_mtime < fs2.st_mtime)
             {
-                if(fs1.st_mtime.tv_nsec){
-
-                }
                 my_linked_list_swap(&(temp1->name), &(temp2->name));
             }
+            
             temp2 = temp2->next;
         }
         temp1 = temp1->next;
     }
 }
 
-void print_linked_list(list* head, int flag){
-    list* current = head;
-    while(current){
-        if(flag == 0 || flag == 2){
-            if(current->name[0] != '.'){
+void print_linked_list(list *head, int flag)
+{
+    list *current = head;
+    while (current)
+    {
+        if (flag == 0 || flag == 2)
+        {
+            if (current->name[0] != '.')
+            {
                 printf("%s\n", current->name);
             }
-        }else{
+        }
+        else
+        {
             printf("%s\n", current->name);
         }
         current = current->next;
     }
 }
 
-void control(input* us_input, int flag){
+void control(input *us_input, int flag)
+{
+    // flag = 0;
     DIR *direct;
     direct = opendir(".");
 
     struct dirent *data;
-    list* content = NULL;
+    list *content = NULL;
     while ((data = readdir(direct)) != NULL)
     {
         add_node(&content, data->d_name);
     }
+
     sort_by_ascii(content);
+
+    if (flag == 1 || flag == 3)
+    {
+        sort_by_ascii(content);
+    }
+    else if (flag == 2)
+    {
+        sort_by_time(content);
+    }
+
     print_linked_list(content, flag);
+
     closedir(direct);
+
+    // print_linked_list(content, flag);
+    // closedir(direct);
 }
 
-int main(int argc, char **argv)
+void options(int argc, char **argv)
 {
     int a = 0;
     int t = 0;
-    input* us_inp = (input*)malloc(sizeof(input));
+    input *us_inp = (input *)malloc(sizeof(input));
     us_inp->files = NULL;
     us_inp->directs = NULL;
     for (int i = 1; i < argc; i++)
@@ -166,7 +192,7 @@ int main(int argc, char **argv)
                 printf("ls: cannot access '%s': No such file or directory\n", argv[i]);
             }
             else
-            { 
+            {
                 struct stat mode;
                 stat(argv[i], &mode);
                 if (S_ISREG(mode.st_mode))
@@ -184,7 +210,6 @@ int main(int argc, char **argv)
     if (a && t)
     {
         flag = 3;
-        // sort_linked_list(all);
     }
     else if (a)
     {
@@ -195,5 +220,10 @@ int main(int argc, char **argv)
         flag = 2;
     }
     control(us_inp, flag);
+}
+
+int main(int argc, char **argv)
+{
+    options(argc, argv);
     return 0;
 }
